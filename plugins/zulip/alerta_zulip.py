a1ce6e799ac4eb6307c0b600e12438aa00674f6a
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-06-29 15:53:21 +0300

    Merge branch 'master' of C:\Users\ansideln\PycharmProjects\alerta with conflicts.

--- plugins/zulip/alerta_zulip.py
+++ plugins/zulip/alerta_zulip.py
@@ -1,24 +1,18 @@
 import logging
 import os
+from datetime import datetime
+
+import zulip
+from jinja2 import Template, UndefinedError
+
+from zulipbot.config.static_config import DATABASE, DB_ROWS
+from zulipbot.database import DBHelper
 
 try:
     from alerta.plugins import app  # alerta >= 5.0
 except ImportError:
     from alerta.app import app  # alerta < 5.0
 from alerta.plugins import PluginBase
-
-import zulip
-from jinja2 import Template, UndefinedError
-
-
-DEFAULT_TMPL = """
-{% if customer %}Customer: `{{customer}}` {% endif %}
-*[{{ status.capitalize() }}] {{ environment }} {{ severity.capitalize() }}*
-{{ event }} {{ resource.capitalize() }}
-```
-{{ text }}
-```
-"""
 
 LOG = logging.getLogger('alerta.plugins.zulip')
 
@@ -33,17 +27,36 @@ ZULIP_TYPE = app.config.get('ZULIP_TYPE') \
 ZULIP_TO = app.config.get('ZULIP_TO') \
            or os.environ.get('ZULIP_TO')
 ZULIP_SUBJECT = app.config.get('ZULIP_SUBJECT') \
-             or os.environ.get('ZULIP_SUBJECT', 'Alert')
+                or os.environ.get('ZULIP_SUBJECT', 'APImon alert')
 ZULIP_ALLOW_UNSECURE = app.config.get('ZULIP_ALLOW_UNSECURE') \
                        or os.environ.get('ZULIP_ALLOW_UNSECURE')
-ZULIP_TEMPLATE = app.config.get('ZULIP_TEMPLATE') \
-                 or os.environ.get('ZULIP_TEMPLATE')
-ZULIP_SERVICE_TOPIC_MAP = app.config.get('ZULIP_SERVICE_TOPIC_MAP')
+ZULIP_REPEAT_INTERVAL = app.config.get('ZULIP_REPEAT_INTERVAL') \
+                        or os.environ.get('ZULIP_REPEAT_INTERVAL', 5)
+DATABASE_URL = app.config.get('DATABASE_URL') \
+               or os.environ.get('DATABASE_URL')
 
 
-class ZulipBot(PluginBase):
+DEFAULT_TMPL = """
+{% if customer %}Customer: `{{customer}}` {% endif %}
+*[{{ status.capitalize() }}] {{ environment }} {{ severity.capitalize() }}*
+{{ event }} {{ resource.capitalize() }}
+```
+{{ text }}
+```
+"""
+
+
+class ZulipBot(PluginBase):  # PluginBase
 
     def __init__(self, name=None):
+        self.connection_string = DATABASE_URL
+        self.db = DBHelper(self.connection_string)
+        self.structure = DATABASE
+        self.db_data = DB_ROWS
+        self.ZULIP_TEMPLATES = {}
+        self.ZULIP_SERVICE_TOPIC_MAP = {}
+        self.SKIP_MAP = []
+
         zulip_args = {
             'site': ZULIP_SITE,
             'email': ZULIP_EMAIL,
@@ -53,27 +66,37 @@ class ZulipBot(PluginBase):
             zulip_args['insecure'] = ZULIP_ALLOW_UNSECURE
         self.bot = zulip.Client(**zulip_args)
 
+        self.db.__connect__()
+        for item in self.structure.values():
+            self.db.query(item.params)
+        for item in self.db_data.values():
+            self.db.query(item.params)
+        self.db.__disconnect__()
         super(ZulipBot, self).__init__(name)
-
-        if ZULIP_TEMPLATE:
-            if os.path.exists(ZULIP_TEMPLATE):
-                with open(ZULIP_TEMPLATE, 'r') as f:
-                    self.template = Template(f.read())
-            else:
-                self.template = Template(ZULIP_TEMPLATE)
-        else:
-            self.template = Template(DEFAULT_TMPL)
 
     def pre_receive(self, alert):
         return alert
 
     def post_receive(self, alert):
-        if alert.repeat and alert.status in ['ack', 'blackout', 'closed']:
-            # skip sending message if original alert ACKed
+        template_name = '_'.join(alert.service)
+
+        self.check_updates()
+        breakpoint()
+        if alert.status in ['ack', 'blackout', 'closed']:
             return
+        elif alert.repeat and delta_minutes(alert.last_receive_time) <= ZULIP_REPEAT_INTERVAL:
+            return
+        for item in self.SKIP_MAP:
+            if alert.environment == item.environment and template_name == item.topic and item.skip is True:
+                return
+
+        if template_name in self.ZULIP_TEMPLATES:
+            template = Template(self.ZULIP_TEMPLATES[template_name])
+        else:
+            template = Template(DEFAULT_TMPL)
 
         try:
-            text = self.template.render(alert.__dict__)
+            text = template.render(alert.__dict__)
         except UndefinedError:
             text = "Can't render zulip template message."
 
@@ -82,20 +105,12 @@ class ZulipBot(PluginBase):
         try:
             message_to = None
             message_subject = None
-            if (ZULIP_SERVICE_TOPIC_MAP
-                    and isinstance(ZULIP_SERVICE_TOPIC_MAP, dict)):
-                # ZULIP_SERVICE_TOPIC_MAP is a dict in the form
-                # {'service1':
-                #    {'to': 'stream_name', 'subject': 'topic_name'}
-                # }
-                for srv in alert.service:
-                    if srv in ZULIP_SERVICE_TOPIC_MAP:
-                        val = ZULIP_SERVICE_TOPIC_MAP[srv]
-                        if 'subject' in val:
-                            message_subject = val['subject']
-                        if 'to' in val:
-                            message_to = val['to']
-                        break
+            if (self.ZULIP_SERVICE_TOPIC_MAP
+                    and isinstance(self.ZULIP_SERVICE_TOPIC_MAP, dict)):
+                if template_name in self.ZULIP_SERVICE_TOPIC_MAP:
+                    val = self.ZULIP_SERVICE_TOPIC_MAP[template_name]
+                    message_subject = val.subject
+                    message_to = val.to
 
             if not message_to:
                 message_to = ZULIP_TO
@@ -124,3 +139,17 @@ class ZulipBot(PluginBase):
 
     def status_change(self, alert, status, text):
         return
+
+    def check_updates(self):
+        """Load/Re-Load Alerta configuration Topics and Templates"""
+        self.db.__connect__()
+        self.ZULIP_TEMPLATES = self.db.get_zulip_templates()
+        self.ZULIP_SERVICE_TOPIC_MAP = self.db.get_topics()
+        self.SKIP_MAP = self.db.get_skip_list()
+        self.db.__disconnect__()
+
+
+def delta_minutes(last_receive_time) -> int:
+    if last_receive_time is None:
+        return 0
+    return (datetime.utcnow().timestamp() - last_receive_time.timestamp()) / 60
